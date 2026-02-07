45fe0cdc7cfd726e05b128673952726628a54227
zalven-official <zalvendayao888@gmail.com>
2026-02-07 14:11:19 +0800

    feat: adding summary event pleace and time

--- lib/utils.ts
+++ lib/utils.ts
@@ -47,10 +47,6 @@ export const formatDateTime = (date: Date) => {
 export function isSameDayIgnoreTimezone(date1: string | Date, date2: string | Date): boolean {
   const d1 = new Date(date1);
   const d2 = new Date(date2);
-
-  return (
-    d1.getUTCFullYear() === d2.getUTCFullYear() &&
-    d1.getUTCMonth() === d2.getUTCMonth() &&
-    d1.getUTCDate() === d2.getUTCDate()
-  );
+  const diff = Math.abs(d1.getTime() - d2.getTime());
+  return diff < 24 * 60 * 60 * 1000;
 }
