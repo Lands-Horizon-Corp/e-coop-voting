e1eaeb9e29f8c9f5eae9e6a04e1f43440bd985ba
Jervx <louellagracechua@gmail.com>
2026-03-13 15:02:43 +0800

    chore: Enhance filtering on public event

--- app/api/v1/public/event/route.ts
+++ app/api/v1/public/event/route.ts
@@ -5,24 +5,47 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { routeErrorHandler } from "@/errors/route-error-handler";
 import { currentUserOrFalse } from "@/lib/auth";
-import { Event } from "@prisma/client";
+import { Event, Role } from "@prisma/client";
+
+type TConditionSet = {
+    roles: Role[];
+    condition: {
+        coopId?: number;
+        branchId?: number;
+    };
+};
 
 export const GET = async (req: NextRequest) => {
     try {
         const currentUser = await currentUserOrFalse();
         let events: Event[] = [];
 
-        if (currentUser) {
-            events = await db.event.findMany({
-                where: { deleted: false, branchId: currentUser.branchId },
-                orderBy: [{ createdAt: "desc" }, { date: "desc" }],
-            });
-        } else {
-            events = await db.event.findMany({
-                where: { deleted: false },
-                orderBy: [{ createdAt: "desc" }, { date: "desc" }],
-            });
-        }
+        const getCondition = () => {
+            if (!currentUser) return {};
+
+            const conditionSet: TConditionSet[] = [
+                { roles: [Role.root], condition: {} },
+                {
+                    roles: [Role.coop_root],
+                    condition: { coopId: currentUser.coopId },
+                },
+                {
+                    roles: [Role.admin, Role.staff],
+                    condition: { branchId: currentUser.branchId },
+                },
+            ];
+
+            const filteredCondition = conditionSet.find((condition) =>
+                condition.roles.includes(currentUser.role),
+            );
+
+            return filteredCondition ? filteredCondition.condition : {};
+        };
+
+        events = await db.event.findMany({
+            where: { deleted: false, ...getCondition() },
+            orderBy: [{ createdAt: "desc" }, { date: "desc" }],
+        });
 
         return NextResponse.json(events);
     } catch (e) {
