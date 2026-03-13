1eb6b5d764a9fbf0ab34ad04ab982b4b5819e687
Jervx <louellagracechua@gmail.com>
2026-03-13 14:39:33 +0800

    chore: Add filter if user signed in, only events related to own branch

--- app/api/v1/public/event/route.ts
+++ app/api/v1/public/event/route.ts
@@ -1,24 +1,33 @@
 export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
+import db from "@/lib/database";
 import db from "@/lib/database";
 import { NextRequest, NextResponse } from "next/server";
 
 import { routeErrorHandler } from "@/errors/route-error-handler";
-import { useSession } from "next-auth/react";
+import { currentUserOrFalse } from "@/lib/auth";
+import { Event } from "@prisma/client";
 
 export const GET = async (req: NextRequest) => {
-  try {
-    const { data: userData } = useSession();
-    if (userData) {
-      // dito event ng sakanya lng desu
-    }
-    const events = await db.event.findMany({
-      where: { deleted: false },
-      orderBy: [{ createdAt: "desc" }, { date: "desc" }],
-    });
+    try {
+        const currentUser = await currentUserOrFalse();
+        let events: Event[] = [];
 
-    return NextResponse.json(events);
-  } catch (e) {
-    return routeErrorHandler(e, req);
-  }
+        if (currentUser) {
+            events = await db.event.findMany({
+                where: { deleted: false, branchId: currentUser.branchId },
+                orderBy: [{ createdAt: "desc" }, { date: "desc" }],
+            });
+        } else {
+            events = await db.event.findMany({
+                where: { deleted: false },
+                orderBy: [{ createdAt: "desc" }, { date: "desc" }],
+            });
+        }
+
+        return NextResponse.json(events);
+    } catch (e) {
+        return routeErrorHandler(e, req);
+    }
 };
