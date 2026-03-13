e159bdca8a9dd12a9a4079c9944bb77a214d8807
zalven-official <zalvendayao888@gmail.com>
2026-03-13 14:37:55 +0800

    fix: unique apply history rows

--- app/api/v1/public/event/route.ts
+++ app/api/v1/public/event/route.ts
@@ -1,19 +1,24 @@
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
-import db from "@/lib/database"
+import db from "@/lib/database";
 import { NextRequest, NextResponse } from "next/server";
 
 import { routeErrorHandler } from "@/errors/route-error-handler";
+import { useSession } from "next-auth/react";
 
-export const GET = async ( req : NextRequest ) => {
-    try{
-        const events = await db.event.findMany({
-            where: { deleted: false },
-            orderBy: [ { createdAt: "desc"} , {date : "desc" } ]
-        })
-
-        return NextResponse.json(events)
-    }catch(e){
-        return routeErrorHandler(e, req)
+export const GET = async (req: NextRequest) => {
+  try {
+    const { data: userData } = useSession();
+    if (userData) {
+      // dito event ng sakanya lng desu
     }
-}
+    const events = await db.event.findMany({
+      where: { deleted: false },
+      orderBy: [{ createdAt: "desc" }, { date: "desc" }],
+    });
+
+    return NextResponse.json(events);
+  } catch (e) {
+    return routeErrorHandler(e, req);
+  }
+};
