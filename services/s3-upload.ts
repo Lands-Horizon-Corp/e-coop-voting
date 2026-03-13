892bc87b2893efe21bfd0a600b9bc01e92bdd1ea
Jervx <louellagracechua@gmail.com>
2026-03-13 09:16:49 +0800

    fix: Fix uploader file path

--- services/s3-upload.ts
+++ services/s3-upload.ts
@@ -7,25 +7,20 @@ import { TFolderUploadGroups } from "@/types";
 import s3Client, { getS3BaseURL } from "@/lib/aws-s3";
 
 export const uploadFile = async (
-  file: Buffer | Readable,
-  originalFileName: string,
-  folder: TFolderUploadGroups,
-  type?: string,
+    file: Buffer | Readable,
+    fileName: string,
+    folder: TFolderUploadGroups,
+    type?: string,
 ) => {
-  const extension = path.extname(originalFileName);
-  const uniqueId = randomUUID();
-  const shardedPath = `${uniqueId.substring(0, 2)}/${uniqueId.substring(2, 4)}`;
-  const finalKey = `${folder}/${shardedPath}/${uniqueId}${extension}`;
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `/${folder}/${fileName}`,
+        Body: file, // Directly use file (Buffer or Readable stream)
+        ContentType: type,
+    };
 
-  const params = {
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: finalKey,
-    Body: file,
-    ContentType: type,
-  };
+    const command = new PutObjectCommand(params);
+    await s3Client.send(command);
 
-  const command = new PutObjectCommand(params);
-  await s3Client.send(command);
-
-  return new URL(params.Key, getS3BaseURL()).href;
+    return new URL(params.Key, getS3BaseURL()).href;
 };
